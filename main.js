48b1c61fa1020082b1b554c790cce2523c393506
DimaLetnianchyn <144311114+DimaLetnianchyn@users.noreply.github.com>
2023-10-04 15:53:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+fs.readFile("data.json", (err, data) => {
+  if (err) {
+    console.log("Помилка зчитування файлу", err);
+    return;
+  }
+
+  let result = '';
+
+  try {
+    const jsonData = JSON.parse(data);
+
+    const selectedData = jsonData.filter(item => item.txt === 'Доходи, усього' || item.txt === 'Витрати, усього');
+
+    for (const item of selectedData) {
+      result += `${item.txt}: ${item.value}\n`; // Виправлено рядок
+    }
+  } catch (error) {
+    console.log('Помилка під час обробки даних:', error);
+    return;
+  }
+
+  fs.writeFile('output.txt', result, (err) => {
+    if (err) {
+      console.log('Помилка в записуванні файлу:', err);
+    } else {
+      console.log('Дані збережено в output.txt');
+    }
+  });
+});
